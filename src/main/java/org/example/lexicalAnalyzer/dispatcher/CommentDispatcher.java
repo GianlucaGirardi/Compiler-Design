db0e7e04fee5c073db1db03f0fdec7389e9625cb
Gianluca Girardi <gl.girardi1@gmail.com>
2026-01-23 18:11:37 -0500

    CP-Lexer: Added Comment and Number disptchers

--- src/main/java/org/example/lexicalAnalyzer/dispatcher/CommentDispatcher.java
+++ src/main/java/org/example/lexicalAnalyzer/dispatcher/CommentDispatcher.java
@@ -1,21 +1,118 @@
 package org.example.lexicalAnalyzer.dispatcher;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.example.lexicalAnalyzer.CharStream;
+import org.example.lexicalAnalyzer.config.DfaRegistry;
+import org.example.lexicalAnalyzer.config.TransitionTable;
 import org.example.lexicalAnalyzer.token.Token;
 
+import static org.example.lexicalAnalyzer.config.Constants.*;
+
+@Data
+@RequiredArgsConstructor
 public class CommentDispatcher implements Dispatcher{
+
+    private final String dfaName;
+
+    private TransitionTable table;
+
+    private int lastRecordedLine;
+
+    private CharStream stream;
+
     @Override
     public Token processToken(CharStream stream) {
+        TransitionTable table = initTable();
+        this.setLastRecordedLine(stream.getLine());
+        this.setStream(stream);
+
+        ScanResult result = consumeCommentLexeme(stream, table);
+        String lexeme = result.lexeme();
+
+        if (lexeme.isEmpty()) return null;
+
+        if (result.forcedInvalid()) {
+            return new Token(getInvalidTokenType(null), lexeme, lastRecordedLine, false);
+        }
+
+        String type = table.getStateToType().get(result.endState());
+        if (type == null) type = COMMENT_NAME;
+
+        return new Token(type, lexeme, lastRecordedLine, true);
+    }
+
+    private ScanResult consumeCommentLexeme(CharStream stream, TransitionTable table) {
+        StringBuilder sb = new StringBuilder();
+        boolean forcedInvalid = false;
+
+        boolean isInline = false;
+        boolean isMultiline = false;
+
+        while (!stream.isAtEnd()) {
+            char c = stream.peek();
+
+            /* Handle inline comment */
+            if (isInline && (c == '\n' || c == '\r')) {
+                break;
+            }
+
+            String charClass = classify(c);
+
+            /* transition only on SLASH or STAR if it is in Transition map */
+            if (charClass != null && table.hasTransition(table.getCurrentState(), charClass)) {
+                table.nextState(table.getCurrentState(), charClass);
+            }
+
+            /* consume && ignore non-driving inputs */
+            sb.append(c);
+            stream.advance();
+
+            /* validate comment type */
+            String stateType = table.getStateToType().get(table.getCurrentState());
+            if (INLINE_COMMENT_NAME.equals(stateType)) {
+                isInline = true;
+            } else if (MULTI_LINE_COMMENT_NAME.equals(stateType)) {
+                isMultiline = true;
+            }
+
+            /* handle terminating multiline */
+            if (isMultiline && table.isAcceptState(table.getCurrentState())) {
+                break;
+            }
+        }
+
+        /* handle non-terminating multiline */
+        if (isMultiline && !table.isAcceptState(table.getCurrentState())) {
+            forcedInvalid = true;
+        }
+
+        return new ScanResult(sb.toString(), table.getCurrentState(), forcedInvalid);
+    }
+
+    public TransitionTable initTable() {
+        TransitionTable table = DfaRegistry.getTable(dfaName);
+        table.reset();
+        this.setTable(table);
+        return table;
+    }
+
+    public String classify(char c) {
+        if (c == SLASH) return SLASH_NAME;
+        else if (c == STAR) return STAR_NAME;
         return null;
     }
 
     @Override
     public boolean isDelimiter(char c) {
+        if(table.getStateToType().get(table.getCurrentState()).equals(INLINE_COMMENT_NAME)) {
+            return this.getStream().getLine() != this.getLastRecordedLine();
+        }
         return false;
     }
 
     @Override
     public String getInvalidTokenType(String lexeme) {
-        return "";
+        return INVALID_COMMENT_MESSAGE;
     }
 }
--- src/main/java/org/example/lexicalAnalyzer/dispatcher/NumberDispatcher.java
+++ src/main/java/org/example/lexicalAnalyzer/dispatcher/NumberDispatcher.java
@@ -1,23 +1,110 @@
 package org.example.lexicalAnalyzer.dispatcher;
 
+import lombok.RequiredArgsConstructor;
 import org.example.lexicalAnalyzer.CharStream;
+import org.example.lexicalAnalyzer.config.DfaRegistry;
+import org.example.lexicalAnalyzer.config.TransitionTable;
 import org.example.lexicalAnalyzer.token.Token;
 
-public class NumberDispatcher implements Dispatcher{
+import java.util.Set;
+
+import static org.example.lexicalAnalyzer.config.Constants.*;
+
+@RequiredArgsConstructor
+public class NumberDispatcher implements Dispatcher {
+
+    private final String dfaName;
+
+    private static final Set<Character> DELIMITER_SET = Set.of(
+            PLUS, MINUS, STAR, SLASH, LPAREN, RPAREN, LBRACE, RBRACE, LBRACKET, RBRACKET, SEMICOLON,
+            COMMA, COLON);
 
     @Override
     public Token processToken(CharStream stream) {
-        System.out.println("Number");
-        return null;
+        TransitionTable table = initTable();
+        int line = stream.getLine();
+
+        ScanResult result = consumeNumberLexeme(stream, table);
+        String lexeme = result.lexeme();
+        String endState = result.endState();
+
+        if (lexeme.isEmpty()) return null;
+        if (result.forcedInvalid()) return new Token(getInvalidTokenType(null), lexeme, line, false);
+
+        String stateType = table.getStateToType().get(endState);
+        if (stateType != null && table.isAcceptState(endState)){
+            return new Token(stateType, lexeme, line, true);
+        }
+
+        return new Token(getInvalidTokenType(null), lexeme, line, false);
+    }
+
+    public TransitionTable initTable() {
+        TransitionTable table = DfaRegistry.getTable(dfaName);
+        table.reset();
+        return table;
     }
 
     @Override
     public boolean isDelimiter(char c) {
-        return false;
+        return Character.isWhitespace(c) || DELIMITER_SET.contains(c);
     }
 
     @Override
     public String getInvalidTokenType(String lexeme) {
-        return "";
+        return INVALID_NUMBER_MESSAGE;
+    }
+
+    private ScanResult consumeNumberLexeme(CharStream stream, TransitionTable table) {
+        StringBuilder stringBuilder = new StringBuilder();
+        boolean forcedInvalid = false;
+
+        while (!stream.isAtEnd()) {
+            char c = stream.peek();
+            String classifiedSymbol = classify(c);
+            String state = table.getCurrentState();
+
+            if (isInAlphabet(classifiedSymbol) && table.hasTransition(state, classifiedSymbol)) {
+                table.nextState(state, classifiedSymbol);
+                stringBuilder.append(c);
+                stream.advance();
+                continue;
+            }
+
+            /* isEndOfToken */
+            if (isDelimiter(c)) break;
+
+            /* consume invalid tail */
+            forcedInvalid = true;
+            while (!stream.isAtEnd()) {
+                char next = stream.peek();
+
+                if (isDelimiter(next)) break;
+                stringBuilder.append(next);
+                stream.advance();
+            }
+            break;
+        }
+
+        return new ScanResult(stringBuilder.toString(), table.getCurrentState(), forcedInvalid);
+    }
+
+    private String classify(char inputSymbol) {
+        if (inputSymbol == ZERO_DIGIT) return ZERO_DIGIT_NAME;
+        else if (isNonZeroDigit(inputSymbol)) return NONZERO_DIGIT_NAME;
+        else if (inputSymbol == DOT) return DOT_NAME;
+        else if (inputSymbol == PLUS) return PLUS_NAME;
+        else if (inputSymbol == MINUS) return MINUS_NAME;
+        else if (inputSymbol == LOWER_CASE_E) return LOWER_CASE_E_NAME;
+
+        return null;
+    }
+
+    private boolean isNonZeroDigit(char inputSymbol) {
+        return inputSymbol > ZERO_DIGIT && inputSymbol <= NINE_DIGIT;
+    }
+
+    private boolean isInAlphabet(String classifiedSymbol) {
+        return classifiedSymbol!= null;
     }
 }
